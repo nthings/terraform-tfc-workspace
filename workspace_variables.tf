e2c37d66e48095aae752b0fa7b60c92a077be6f2
Mauricio Martinez <n_othing@hotmail.com>
2022-06-15 14:15:52 -0500

    Adding complex types variables

--- workspace_variables.tf
+++ workspace_variables.tf
@@ -1,5 +1,7 @@
 locals {
-  workspace_variables_non_hcl = { for key, value in var.workspace_variables : key => value if can(tostring(value)) || can(tonumber(value)) }
+  workspace_variables_non_hcl   = { for key, value in var.workspace_variables : key => value if can(tostring(value)) || can(tonumber(value)) }
+  workspace_variables_maps_hcl  = { for key, value in var.workspace_variables : key => value if can(tomap(value)) }
+  workspace_variables_lists_hcl = { for key, value in var.workspace_variables : key => value if can(tolist(value)) }
 }
 
 resource "tfe_variable" "workspace_variables_non_hcl" {
@@ -12,18 +14,28 @@ resource "tfe_variable" "workspace_variables_non_hcl" {
   sensitive    = false
 }
 
-# resource "tfe_variable" "workspace_variables_hcl" {
-#   for_each     = { for key, value in var.workspace_variables : key => value if !can(tostring(value)) }
-#   key          = each.key
-#   value        = <<EOT
-#                   {
-#                     %{for key, value in each.value~}
-#                     ${key} = "${value}"
-#                     %{endfor~}
-#                   }
-#                   EOT
-#   category     = "terraform"
-#   hcl          = true
-#   workspace_id = tfe_workspace.this.id
-#   sensitive    = false
-# }
+resource "tfe_variable" "workspace_variables_maps_hcl" {
+  for_each     = local.workspace_variables_maps_hcl
+  key          = each.key
+  value        = <<EOT
+                  {
+                    %{for key, value in each.value~}
+                    ${key} = "${value}"
+                    %{endfor~}
+                  }
+                  EOT
+  category     = "terraform"
+  hcl          = true
+  workspace_id = tfe_workspace.this.id
+  sensitive    = false
+}
+
+resource "tfe_variable" "workspace_variables_lists_hcl" {
+  for_each     = local.workspace_variables_lists_hcl
+  key          = each.key
+  value        = jsonencode(each.value)
+  category     = "terraform"
+  hcl          = true
+  workspace_id = tfe_workspace.this.id
+  sensitive    = false
+}
